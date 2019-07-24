103ed984623b6b2144d5a805a748104779ef29d9
shmeegie <ericellb@gmail.com>
2019-07-24 01:35:56 -0400

    Added Tests Boilerplates -> Completed Actions Test

---
+++ src/__tests__/actions.test.js
@@ -0,0 +1,73 @@
+import { FETCH_RECIPES, SIGN_IN, SIGN_OUT } from '../actions/types';
+import moxios from 'moxios';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+
+const middleware = [thunk];
+const mockStore = configureMockStore(middleware);
+
+describe('Actions Test', () => {
+
+  beforeEach(() => {
+    moxios.install();
+  })
+
+  afterEach(() => {
+    moxios.uninstall();
+  })
+
+  it('should return a SIGN_IN action with payload of userId', () => {
+
+    const response = {
+      type: SIGN_IN,
+      payload: 1
+    }
+
+    const userId = response.userId;
+    expect(signIn(userId)).toEqual(res);
+
+  });
+
+  it('should return a SIGN_OUT action with a payload of userId', () => {
+    const response = {
+      type: SIGN_OUT,
+      payload: 1
+    }
+
+    const userId = response.userId;
+    expect(signOut(userId)).toEqual(res);
+  })
+
+  it('should return a FETCH_STREAMS action with a payload of the API Recipe Data', () => {
+
+    const mockData = {
+      1: {
+        recipe: 'Chicken Parm'
+      },
+      2: {
+        recipe: 'Lasagna'
+      }
+    }
+
+    const expectedAction = {
+      type: FETCH_RECIPES,
+      payload: mockData
+    }
+
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: mockData
+      });
+    });
+
+    const store = mockStore({ recipes: {} });
+    return store.dispatch(fetchRecipes()).then(() => {
+      expect(store.getActions()).toEqual(expectedAction);
+    })
+
+  })
+
+})
---
+++ src/actions/types.js
@@ -0,0 +1,3 @@
+export const FETCH_RECIPES = 'FETCH_RECIPES';
+export const SIGN_IN = 'SIGN_IN';
+export const SIGN_OUT = 'SIGN_OUT';
