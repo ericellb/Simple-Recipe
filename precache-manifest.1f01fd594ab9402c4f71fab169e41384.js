ea3a93b818e2874ffe4b1d30fd24cb9666cca293
shmeegie <ericellb@gmail.com>
2019-08-01 18:40:41 -0400

    Updates

---
+++ precache-manifest.1f01fd594ab9402c4f71fab169e41384.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32ffc5a36a33db84f7fdeeb7d5c65189",
+    "url": "/simple-recipe/index.html"
+  },
+  {
+    "revision": "5ad0a3550ac6275536eb",
+    "url": "/simple-recipe/static/css/2.75a4e6fe.chunk.css"
+  },
+  {
+    "revision": "4fa6a072faf4d5495c25",
+    "url": "/simple-recipe/static/css/main.229e5662.chunk.css"
+  },
+  {
+    "revision": "5ad0a3550ac6275536eb",
+    "url": "/simple-recipe/static/js/2.cdb58791.chunk.js"
+  },
+  {
+    "revision": "4fa6a072faf4d5495c25",
+    "url": "/simple-recipe/static/js/main.bc15cb0a.chunk.js"
+  },
+  {
+    "revision": "6149a38194babd922622",
+    "url": "/simple-recipe/static/js/runtime~main.a9e32fc6.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/simple-recipe/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/simple-recipe/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/simple-recipe/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/simple-recipe/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/simple-recipe/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/simple-recipe/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/simple-recipe/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/simple-recipe/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/simple-recipe/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/simple-recipe/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/simple-recipe/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/simple-recipe/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/simple-recipe/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/simple-recipe/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/simple-recipe/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/simple-recipe/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
